c1c13945e89484cbcaa844192a791abd83d9c322
Mubarak Ganiyu <mubarak.a.ganiyu@vanderbilt.edu>
2023-08-27 17:13:10 -0500

    Update ask-data.py

--- ask-data.py
+++ ask-data.py
@@ -12,7 +12,7 @@ st.title('🦜🔗 Ask the Data App')
 def load_file(input_file):
   if input_file[-4:] == ".csv":
       df = pd.read_csv(input_file)
-  elif input_file[-5:] == ".xlsx"
+  elif input_file[-5:] == ".xlsx":
       df = pd.read_excel(input_file)
   with st.expander('See DataFrame'):
     st.write(df)
